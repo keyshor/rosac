c55057f36ea3d461ee1aa0c427efbc7c6b225d51
Steve Hsu <triangular.pyramid@gmail.com>
2021-02-09 23:12:50 -0500

    wrote API from Google doc

---
+++ mode.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python
+
+# typing annotations written for Python 3.8
+import numpy as np
+from typing import Tuple, Iterable, Iterator, Callable, Optional
+import itertools
+
+c: Iterator[int] = itertools.count()
+
+# the Mode class just wraps user-provided functions with run-time type checks
+class Mode:
+    # callable fields marked as optional
+    # workaround for https://github.com/python/mypy/issues/9489
+    id: str
+    state_shape: Tuple[int, ...]
+    action_shape: Tuple[int, ...]
+    observation_shape: Tuple[int, ...]
+    # reset: () -> state
+    reset_callable: Optional[Callable[[np.random.Generator], np.ndarray]]
+    # step: state * action -> state
+    step_callable: Optional[Callable[[np.ndarray, np.ndarray], np.ndarray]]
+    # observe: state -> observation
+    observe_callable: Optional[Callable[[np.ndarray], np.ndarray]]
+    # reward: state] -> float
+    reward_callable: Optional[Callable[[np.ndarray], float]]
+    # is_safe: state -> bool
+    is_safe_callable: Optional[Callable[[np.ndarray], bool]]
+    # is_done: state -> bool
+    is_done_callable: Optional[Callable[[np.ndarray], bool]]
+    # render: state -> None
+    render_callable: Optional[Callable[[np.ndarray], None]]
+
+    def __init__(self,
+            id: str,
+            state_shape: Iterable[int],
+            action_shape: Iterable[int],
+            observation_shape: Iterable[int],
+            reset: Callable[[np.random.Generator], np.ndarray],
+            step: Callable[[np.ndarray, np.ndarray], np.ndarray],
+            observe: Callable[[np.ndarray], np.ndarray],
+            reward: Callable[[np.ndarray], float],
+            is_safe: Callable[[np.ndarray], bool],
+            is_done: Callable[[np.ndarray], bool],
+            render: Callable[[np.ndarray], None]
+            ) -> None:
+        self.id = f'{id}_m{next(c)}'
+        state_shape = tuple(state_shape)
+        action_shape = tuple(action_shape)
+        observation_shape = tuple(observation_shape)
+        self.reset_callable = reset
+        self.step_callable = step
+        self.observe_callable = observe
+        self.reward_callable = reward
+        self.is_safe_callable = is_safe
+        self.is_done_callable = is_done
+        self.render_callable = render
+
+    # reset: () -> state
+    def reset(self, rng: np.random.Generator) -> np.ndarray:
+        assert self.reset_callable is not None
+        new_state = self.reset_callable(rng)
+        assert new_state.dtype.kind == 'f' \
+                and new_state.shape == self.state_shape
+        return new_state
+
+    # step: state * action -> state
+    def step(self, old_state: np.ndarray, action: np.ndarray) -> np.ndarray:
+        assert self.step_callable is not None
+        assert old_state.dtype.kind == 'f' \
+                and old_state.shape == self.state_shape
+        assert action.dtype.kind == 'f' \
+                and action.shape == self.action_shape
+        new_state = self.step_callable(old_state, action)
+        assert new_state.dtype.kind == 'f' \
+                and new_state.shape == self.state_shape
+        return new_state
+
+    # observe: state -> observation
+    def observe(self, state: np.ndarray) -> np.ndarray:
+        assert self.observe_callable is not None
+        assert state.dtype.kind == 'f' \
+                and state.shape == self.state_shape
+        observation = self.observe_callable(state)
+        assert observation.dtype.kind == 'f' \
+                and observation.shape == self.observation_shape
+        return observation
+
+    # reward: state -> float
+    def reward(self, state: np.ndarray) -> float:
+        assert self.reward_callable is not None
+        assert state.dtype.kind == 'f' \
+                and state.shape == self.state_shape
+        return self.reward_callable(state)
+
+    # is_safe: state -> bool
+    def is_safe(self, state: np.ndarray) -> bool:
+        assert self.is_safe_callable is not None
+        assert state.dtype.kind == 'f' \
+                and state.shape == self.state_shape
+        return self.is_safe_callable(state)
+
+    # is_done: state -> bool
+    def is_done(self, state: np.ndarray) -> bool:
+        assert self.is_done_callable is not None
+        assert state.dtype.kind == 'f' \
+                and state.shape == self.state_shape
+        return self.is_done_callable(state)
+
+    # render: state -> None
+    def render(self, state: np.ndarray) -> None:
+        assert self.render_callable is not None
+        assert state.dtype.kind == 'f' \
+                and state.shape == self.state_shape
+        self.render_callable(state)
---
+++ transition.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+# typing annotations written for Python 3.8
+import numpy as np
+from typing import List, Dict, Tuple, Iterable, Mapping, Callable, Optional
+
+class Transition:
+    # callable fields marked as optional
+    # workaround for https://github.com/python/mypy/issues/9489
+    state_shape: Tuple[int, ...]
+    guard_callable: Optional[Callable[[np.ndarray], bool]]
+    source: str
+    targets: List[str]
+    probabilities: List[float]
+    jump_callables: Dict[str, Callable[[np.ndarray], np.ndarray]]
+
+    def __init__(self,
+        state_shape: Iterable[int],
+            guard_callable: Callable[[np.ndarray], bool],
+            source: str,
+            targets: Iterable[str],
+            jump_callables: Iterable[Callable[[np.ndarray], np.ndarray]],
+            probabilities: Iterable[float],
+            ) -> None:
+        self.state_shape = tuple(state_shape)
+        self.guard_callable = guard_callable
+        self.source = source
+        self.targets = list(targets)
+        jump_callable_list = list(jump_callables)
+        probabilities_unnormalized = list(probabilities)
+        assert len(self.targets) == len(jump_callable_list) \
+                == len(probabilities_unnormalized)
+        self.jump_callables = {
+                self.targets[i] : jump_callable_list[i]
+                for i in range(len(self.targets))
+                }
+        assert all([p >= 0 for p in probabilities_unnormalized])
+        sum_probs = sum(probabilities_unnormalized)
+        if abs(sum_probs - 1.0) < 0.001:
+            self.probabilities = probabilities_unnormalized
+        else:
+            self.probabilities = [p / sum_probs
+                    for p in probabilities_unnormalized]
+
+    def guard(self, state: np.ndarray) -> bool:
+        assert self.guard_callable is not None
+        assert state.dtype.kind == 'f' \
+                and state.shape == self.state_shape
+        return self.guard_callable(state)
+
+    # jump: state -> str * state
+    def jump(self, old_state: np.ndarray, rng: np.random.Generator) \
+            -> Tuple[str, np.ndarray]:
+        assert old_state.dtype.kind == 'f' \
+                and old_state.shape == self.state_shape
+        new_mode = rng.choice(self.targets, p=self.probabilities)
+        new_state = self.jump_callables[new_mode](old_state)
+        assert new_state.dtype.kind == 'f' \
+                and new_state.shape == self.state_shape
+        return new_mode, new_state
