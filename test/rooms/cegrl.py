a4240cfe69146402a5cfe582051d693d99e31f13
keyshor <kishor1996@gmail.com>
2021-09-23 13:44:16 -0400

    increasing max jumps

--- test/rooms/cegrl.py
+++ test/rooms/cegrl.py
@@ -50,13 +50,13 @@ if __name__ == '__main__':
         num_synth_iter = 10
 
     nn_params = NNParams(2, 2, 1.0, 32)
-    ars_params = ARSParams(250, 30, 15, 0.05, 0.3, 0.95, 25)
+    ars_params = ARSParams(300, 30, 15, 0.05, 0.3, 0.95, 25)
 
     controllers, log_info = cegrl(automaton, pre, time_limits, num_iter=100, num_synth_iter=num_synth_iter,
                                   abstract_synth_samples=flags['abstract_samples'], print_debug=True,
                                   use_best_model=flags['best'], falsify_func=falsify_func,
                                   save_path=flags['path'], algo_name='ars', nn_params=nn_params,
-                                  ars_params=ars_params, use_gpu=flags['gpu'], max_jumps=5)
+                                  ars_params=ars_params, use_gpu=flags['gpu'], max_jumps=15)
 
     # save the controllers
     for (mode_name, ctrl) in controllers.items():
