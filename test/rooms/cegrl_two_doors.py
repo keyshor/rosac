61ce741285037785ca9060f145aef493e9362aa9
keyshor <kishor1996@gmail.com>
2022-02-03 20:27:46 -0500

    changed exploration for two doors

--- test/rooms/cegrl_two_doors.py
+++ test/rooms/cegrl_two_doors.py
@@ -55,7 +55,7 @@ if __name__ == '__main__':
                              actor_hidden_dim=64, critic_hidden_dim=64, max_timesteps=25,
                              test_max_timesteps=25, sigma=0.15)
     sac_kwargs = dict(hidden_dims=(64, 64), episodes_per_epoch=20, max_ep_len=25, test_ep_len=25,
-                      alpha=0.08, min_alpha=0.04, alpha_decay=0.002,
+                      alpha=0.1, min_alpha=0.04, alpha_decay=0.002,
                       gpu_device='cuda:{}'.format(flags['gpu_num'] % num_gpus))
 
     controllers, log_info = cegrl(automaton, pre, time_limits, num_iter=100, num_synth_iter=num_synth_iter,
